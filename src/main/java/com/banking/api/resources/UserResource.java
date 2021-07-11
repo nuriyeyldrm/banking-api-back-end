b3f98863a94efc4ff3fa564c4f32349aa95a9595
nuriyeyldrm <nuriye.yildirim@std.antalya.edu.tr>
2021-07-11 21:11:34 +0300

    initial commit

--- src/main/java/com/banking/api/resources/UserResource.java
+++ src/main/java/com/banking/api/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.HashMap;
@@ -44,7 +45,7 @@ public class UserResource {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<Map<String, String>> registerUser(@RequestBody Map<String, Object> userMap){
+    public ResponseEntity<Map<String, String>> registerUser(@Valid @RequestBody Map<String, Object> userMap){
         String firstName = (String) userMap.get("firstName");
         String lastName = (String) userMap.get("lastName");
         String email = (String) userMap.get("email");
