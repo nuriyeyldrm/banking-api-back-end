1be0110c94fdf15880a2eae67a2dacc034d82990
nuriyeyldrm <nuriye.yildirim@std.antalya.edu.tr>
2021-07-11 06:48:17 +0300

    initial commit

--- src/main/java/com/banking/api/config/Constants.java
+++ src/main/java/com/banking/api/config/Constants.java
@@ -7,12 +7,6 @@ public final class Constants {
     // token validity is 2 hours, after token will expire
     public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
 
-    public static final String LOGIN_REGEX = "^(?>[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*)|(?>[_.@A-Za-z0-9-]+)$";
-
-    public static final String SYSTEM_ACCOUNT = "system";
-    public static final String DEFAULT_LANGUAGE = "en";
-    public static final String ANONYMOUS_USER = "anonymoususer";
-
     private Constants(){
 
     }
