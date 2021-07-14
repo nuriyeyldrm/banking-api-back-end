2e1994cc90a25bf462b2da1db58928618228d3f6
nuriyeyldrm <nuriye.yildirim@std.antalya.edu.tr>
2021-07-14 12:23:01 +0300

    initial commit

--- src/main/java/com/banking/api/BankingApiBackEndApplication.java
+++ src/main/java/com/banking/api/BankingApiBackEndApplication.java
@@ -30,7 +30,7 @@ public class BankingApiBackEndApplication {
         config.addAllowedMethod("DELETE");
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
-        source.registerCorsConfiguration("/**", config);
+//        source.registerCorsConfiguration("/**", config);
         source.registerCorsConfiguration("localhost:3000", config);
         registrationBean.setFilter(new CorsFilter(source));
         registrationBean.setOrder(0);
@@ -50,18 +50,18 @@ public class BankingApiBackEndApplication {
         return registrationBean;
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowedOrigins("http://localhost:3000");
-
-            }
-        };
-    }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowedHeaders("*")
+//                        .allowedOrigins("http://localhost:3000");
+//
+//            }
+//        };
+//    }
 
 }
